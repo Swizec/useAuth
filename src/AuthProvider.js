008298249f413f3a0c7df6a3caa031e2518706e7
Swizec Teller <swizec@swizec.com>
2019-08-04 17:30:06 -0700

    Getting close to making this work

--- src/AuthProvider.js
+++ src/AuthProvider.js
@@ -1,7 +1,7 @@
-import React, { useReducer } from "react";
+import React from "react";
 import Auth0 from "auth0-js";
 
-// import { authReducer } from "./authReducer";
+import { authReducer } from "./authReducer";
 
 const AUTH0_DOMAIN = "sparkjoy.auth0.com";
 const AUTH0_CLIENT_ID = "GGfO12E5R8iHPBPh87bym5b3gzmdaYY9";
@@ -12,56 +12,18 @@ const CALLBACK_DOMAIN =
 
 export const AuthContext = React.createContext(null);
 
-const authReducer = (state, action) => {
-    switch (action.type) {
-        case "login":
-            const { authResult, user } = action;
-            const expiresAt =
-                authResult.expiresIn * 1000 + new Date().getTime();
-
-            if (typeof localStorage !== "undefined") {
-                localStorage.setItem("access_token", authResult.accessToken);
-                localStorage.setItem("id_token", authResult.idToken);
-                localStorage.setItem("expires_at", JSON.stringify(expiresAt));
-                localStorage.setItem("user", JSON.stringify(user));
-            }
-
-            return {
-                user,
-                expiresAt
-            };
-        case "logout":
-            if (typeof localStorage !== "undefined") {
-                localStorage.removeItem("access_token");
-                localStorage.removeItem("id_token");
-                localStorage.removeItem("expires_at");
-                localStorage.removeItem("user");
-            }
-
-            return {
-                user: {},
-                expiresAt: null
-            };
-        default:
-            return state;
-    }
-};
-
 export const AuthProvider = ({ children, navigate }) => {
-    const [state, dispatch] = useReducer((state, action) => state, {});
-
     const auth0 = new Auth0.WebAuth({
         domain: AUTH0_DOMAIN,
         clientID: AUTH0_CLIENT_ID,
         redirectUri: `${CALLBACK_DOMAIN}/auth0_callback`,
         audience: `https://${AUTH0_DOMAIN}/api/v2/`,
         responseType: "token id_token",
-        scope: "openid profile email"
+        scope: "openidw profile email"
     });
 
-    console.log({ useReducer });
-
-    // const [state, dispatch] = useReducer(authReducer, {
+    // Uncomment this code brings back the 2 Reacts error
+    // const [state, dispatch] = React.useReducer(authReducer, {
     //     user:
     //         typeof localStorage !== "undefined"
     //             ? JSON.parse(localStorage.getItem("user"))
@@ -75,8 +37,8 @@ export const AuthProvider = ({ children, navigate }) => {
     return (
         <AuthContext.Provider
             value={{
-                state,
-                dispatch,
+                // state,
+                // dispatch,
                 auth0,
                 navigate
             }}
