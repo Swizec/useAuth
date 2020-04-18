2cad24eaf30e90c108bf2beda307f306b820dae3
Swizec Teller <swizec@swizec.com>
2020-04-18 16:04:00 -0700

    useAuth tests

--- src/__tests__/useAuth.spec.tsx
+++ src/__tests__/useAuth.spec.tsx
@@ -3,6 +3,7 @@ import ReactRenderer from "react-test-renderer";
 
 import { useAuth, handleAuthResult } from "../useAuth";
 import { AuthContext } from "../AuthProvider";
+import { AuthContextState } from "../types";
 import Auth0 from "auth0-js";
 
 const auth0 = new Auth0.WebAuth({
@@ -14,9 +15,10 @@ const auth0 = new Auth0.WebAuth({
     scope: "openid profile email"
 });
 auth0.authorize = jest.fn();
+auth0.logout = jest.fn();
 
 describe("useAuth", () => {
-    const context = {
+    const context: AuthContextState = {
         state: {
             user: { sub: "1234" },
             expiresAt: null,
@@ -28,6 +30,13 @@ describe("useAuth", () => {
         navigate: jest.fn()
     };
 
+    const render = (context: any, Mock: any) =>
+        ReactRenderer.create(
+            <AuthContext.Provider value={context}>
+                <Mock />
+            </AuthContext.Provider>
+        );
+
     describe("login", () => {
         it("calls auth0.authorize()", () => {
             const Mock = () => {
@@ -37,21 +46,103 @@ describe("useAuth", () => {
                 return null;
             };
 
-            ReactRenderer.create(
-                <AuthContext.Provider value={context}>
-                    <Mock />
-                </AuthContext.Provider>
-            );
+            render(context, Mock);
 
             expect(auth0.authorize).toBeCalled();
         });
     });
 
-    describe("logout", () => {});
+    describe("logout", () => {
+        const Mock = () => {
+            const { logout } = useAuth();
+            logout();
 
-    describe("handleAuthentication", () => {});
+            return null;
+        };
 
-    describe("isAuthenticated", () => {});
+        it("calls auth0.logout()", () => {
+            render(context, Mock);
+
+            expect(auth0.logout).toBeCalledWith({
+                returnTo: context.callback_domain
+            });
+        });
+
+        it("dispatches logout action", () => {
+            render(context, Mock);
+
+            expect(context.dispatch).toBeCalledWith({
+                type: "logout"
+            });
+        });
+    });
+
+    describe("handleAuthentication", () => {
+        const Mock = () => {
+            const { handleAuthentication } = useAuth();
+            handleAuthentication({ postLoginRoute: "/route" });
+
+            return null;
+        };
+
+        it("dispatches startAuthenticating", () => {
+            render(context, Mock);
+
+            expect(context.dispatch).toBeCalledWith({
+                type: "startAuthenticating"
+            });
+        });
+
+        it("navigates to postLoginRoute", () => {
+            render(context, Mock);
+
+            expect(context.navigate).toBeCalledWith("/route");
+        });
+    });
+
+    describe("isAuthenticated", () => {
+        it("is false when expiresAt not set", () => {
+            const Mock = () => {
+                const { isAuthenticated } = useAuth();
+
+                expect(isAuthenticated()).toBe(false);
+
+                return null;
+            };
+
+            context.state.expiresAt = null;
+
+            render(context, Mock);
+        });
+
+        it("is false when expiresAt in the past", () => {
+            const Mock = () => {
+                const { isAuthenticated } = useAuth();
+
+                expect(isAuthenticated()).toBe(false);
+
+                return null;
+            };
+
+            context.state.expiresAt = new Date().getTime() - 3600 * 1000;
+
+            render(context, Mock);
+        });
+
+        it("is true when expiresAt in the future", () => {
+            const Mock = () => {
+                const { isAuthenticated } = useAuth();
+
+                expect(isAuthenticated()).toBe(true);
+
+                return null;
+            };
+
+            context.state.expiresAt = new Date().getTime() + 3600 * 1000;
+
+            render(context, Mock);
+        });
+    });
 });
 
 describe("handleAuthResult", () => {
