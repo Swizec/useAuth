30292310f69693b04c835d1dfe69843e7dea4cf0
Swizec Teller <swizec@swizec.com>
2020-10-04 14:40:07 -0700

    Add jsdoc for useAuth

--- dist/useAuth.d.ts
+++ dist/useAuth.d.ts
@@ -1,3 +1,17 @@
 import { useAuthInterface, handleAuthResultInterface } from "./types";
 export declare const handleAuthResult: handleAuthResultInterface;
+/**
+ * The main API for useAuth
+ *
+ * @return {boolean} isAuthenticated is current user authenticated
+ * @return {boolean} isAuthenticating currently running authentication
+ * @return {function} isAuthorized check if current user is authenticated and matches list of roles
+ * @return {object} user current user
+ * @return {string} userId current user's identifier
+ * @return {object} authResult raw authentication result object from auth provider
+ * @return {function} login start the login process
+ * @return {function} signup same as login, passes { mode: "signUp", screen_hint: "signup" } to Auth0
+ * @return {function} logout start the logout process
+ * @return {function} handleAuthentication function to call on your callback page
+ */
 export declare const useAuth: useAuthInterface;
--- src/useAuth.ts
+++ src/useAuth.ts
@@ -74,6 +74,20 @@ export const handleAuthResult: handleAuthResultInterface = async ({
     }
 };
 
+/**
+ * The main API for useAuth
+ *
+ * @return {boolean} isAuthenticated is current user authenticated
+ * @return {boolean} isAuthenticating currently running authentication
+ * @return {function} isAuthorized check if current user is authenticated and matches list of roles
+ * @return {object} user current user
+ * @return {string} userId current user's identifier
+ * @return {object} authResult raw authentication result object from auth provider
+ * @return {function} login start the login process
+ * @return {function} signup same as login, passes { mode: "signUp", screen_hint: "signup" } to Auth0
+ * @return {function} logout start the logout process
+ * @return {function} handleAuthentication function to call on your callback page
+ */
 export const useAuth: useAuthInterface = () => {
     const {
         state,
