8dfa7b963e900ffe9b8bf665aac7a59db0df4e9f
Swizec Teller <swizec@swizec.com>
2020-12-17 07:41:04 -0800

    Kill <AuthProvider>

--- src/AuthProvider.tsx
+++ src/AuthProvider.tsx
@@ -14,47 +14,9 @@ export const AuthProvider: AuthProviderInterface = ({
     auth0_params = {},
     customPropertyNamespace
 }) => {
-    const callbackDomain =
-        typeof window !== "undefined"
-            ? `${window.location.protocol}//${window.location.host}`
-            : "http://localhost:8000";
-
-    const audienceDomain = auth0_audience_domain || auth0_domain;
-
-    const params: Auth0Options = {
-        domain: auth0_domain,
-        clientID: auth0_client_id,
-        redirectUri: `${callbackDomain}/auth0_callback`,
-        audience: `https://${audienceDomain}/api/v2/`,
-        responseType: "token id_token",
-        scope: "openid profile email"
-    };
-
-    const { dispatch } = useAuth();
-
-    // Instantiate Auth0 client
-
     useEffect(() => {
-        import("./providers/auth0").then(({ Auth0 }) => {
-            const auth0 = new Auth0({
-                dispatch,
-                customPropertyNamespace,
-                ...params,
-                ...auth0_params
-            });
-
-            dispatch("SET_CONFIG", {
-                authProvider: auth0,
-                navigate
-            });
-
-            dispatch("CHECK_SESSION");
-        });
-    }, [navigate, customPropertyNamespace]);
-
-    useEffect(() => {
-        console.warn(
-            "Using the AuthProvider root component is deprecated. Migrate to AuthConfig or manual dispatching. Takes 5min. 👉 https://useauth.dev/docs/upgrading"
+        console.error(
+            "The AuthProvider root component no longer works. Please take 5min to migrate to AuthConfig. 👉 https://useauth.dev/docs/upgrading"
         );
     }, []);
 
